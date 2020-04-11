039e7e440e776d397feee5772567e7e2e8350053
iamonzon <github@iamonzon.dev>
2020-04-11 05:07:47 -0300

    vim global copy

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,8 +7,13 @@ sudo apt-get install -y git
 git config --global color.ui true
 git config --global push.default simple
 
-sudo apt-get install -y git-cola
+application (
+  git-cola
+  vim
+  vim-gnome
+)
+sudo apt-get install -y application{$@}
 
-sudo apt-get install -y vim
+# Zsh. Oh-my-shell. Tilix. Configure all the properties and themes and plugins. copy this files. Change global variables to $DOTFILES
 
 
