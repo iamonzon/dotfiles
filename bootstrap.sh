4c1bd0f659e110abf51ec55473d491b7eaff2a98
iamonzon <github@iamonzon.dev>
2020-04-11 08:47:43 -0300

    logging

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 ## Run this script with sudo ##
+echo Updating packages
 sudo apt-get update
 
+echo Installing and configuring GIT
 sudo apt-get install -y git
 git config --global color.ui true
 git config --global push.default simple
 
+echo Installing applications
 applications=(
   git-cola
   vim
@@ -14,6 +17,7 @@ applications=(
 )
 sudo apt-get install -y ${applications[@]}
 
+echo Installing Shell apps
 shell=(
   zsh
   tilix
@@ -21,12 +25,14 @@ shell=(
 
 sudo apt-get install -y ${shell[@]} 
 
+echo Setting ZSH as SHELL
 #Set zsh as default SHELL
 chsh -s $(which zsh)
 
 #Install oh-my-shell
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+echo Cloning Theme for ZSH
 #Theme
 git clone https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 
