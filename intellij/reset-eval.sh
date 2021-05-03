6325fb28be8eb010269202e87da2363dc2a69170
Capitan Empanada <ivanlp10n2@gmail.com>
2021-05-02 21:18:14 -0300

    intellij reset fixed

--- intellij/reset-eval.sh
+++ intellij/reset-eval.sh
@@ -1,4 +1,3 @@
 rm -rf ~/.config/JetBrains/IntelliJIdea*/eval
-rm -rf ~/.config/JetBrains/PyCharm*/options/other.xml (if exists)
 sed -i -E 's/<property name=\"evl.*\".*\/>//' ~/.config/JetBrains/IntelliJIdea*/options/other.xml
 rm -rf ~/.java/.userPrefs/jetbrains/idea
