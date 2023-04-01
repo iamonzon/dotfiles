3078343272e83d6feba1bb3ac03d4027ede08f69
iamonzon <github@iamonzon.dev>
2023-04-01 01:15:48 -0300

    deleted intellij dependencies

--- intellij/reset-eval-webstorm.sh
+++
@@ -1,3 +0,0 @@
-rm -rf ~/.config/JetBrains/WebStorm*/eval
-sed -i -E 's/<property name=\"evl.*\".*\/>//' ~/.config/JetBrains/WebStorm*/options/other.xml
-rm -rf ~/.java/.userPrefs/jetbrains/webstorm
--- intellij/reset-eval.sh
+++
@@ -1,3 +0,0 @@
-rm -rf ~/.config/JetBrains/IntelliJIdea*/eval
-sed -i -E 's/<property name=\"evl.*\".*\/>//' ~/.config/JetBrains/IntelliJIdea*/options/other.xml
-rm -rf ~/.java/.userPrefs/jetbrains/idea
