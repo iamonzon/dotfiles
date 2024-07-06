694e30e905e48078e6ea3583dee4fcc6e4b216dc
Capitan Empanada <ivanlp10n2@users.noreply.github.com>
2024-07-06 12:20:15 -0700

    Feature/modularizing (#3)

--- macos_functions.sh
+++ macos_functions.sh
@@ -1,63 +1,117 @@
 #!/bin/bash
-function macos_installation {
-  echo =========== Installing brew-package-manager (brew) ===========
-  export HOMEBREW_NO_INSTALL_FROM_API=1
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-  
-  echo =========== Installing applications with brew  =========== 
-  applications=(
-    git
-    nvim
-    curl
-    zsh
-  )
 
-  brew install -y ${applications[@]}
+DOTFILES_PATH=~/.dotfiles
+DOTFILES_ZSH_PATH="$DOTFILES_PATH/zsh"
+DRY_RUN=false
+EXPECTED_HOME="$HOME/expected_home/$(date +%Y%m%d_%H%M%S)"
 
-  echo  =========== GIT configuration ==============
-  git config --global color.ui true
-  git config --global push.default simple
-
-  echo =========== Setting ZSH as SHELL ===========
-  chsh -s $(which zsh)
-
-  echo =========== Installing oh-my-shell ===========
-  sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-  echo =========== Installing plugins for Zsh ===========
-  echo =========== Installing zsh-autosuggestions ===========
-  git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-
-  echo =========== Installing zsh-syntax-highlighting ===========
-  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-
-  echo =========== Installing autojump ===========
-  sudo apt install -y autojump
-
-  DOTFILES_PATH=~/.dotfiles
-  echo =========== Cloning dotfiles in $DOTFILES_PATH to make safe links ============
-  git clone https://github.com/ivanlp10n2/dotfiles $DOTFILES_PATH 
-
-  echo =========== Making safe copies of the files =============
-  #Make safe copies of the files
-  echo =========== Creating backups '.bak' of the files '~/.zshrc, ~/.vimrc, ~/.vim ~/.nvim' =============
-  mv ~/.zshrc ~/.bak.zshrc
-  mv ~/.vimrc ~/.bak.vimrc
-  mv ~/.vim ~/.bak.vim
-  mv ~/.config/nvim ~/.config/.bak.nvim
-
-  echo =========== Linking files ===========
-  mkdir -p ~/.config/
-
-  ln -s $DOTFILES_PATH/zsh/zshrc ~/.zshrc
-  ln -s $DOTFILES_PATH/vim/.vimrc ~/.vimrc
-  ln -s $DOTFILES_PATH/vim/.vim/ ~/.vim/
-
-  echo =========== Copying git-pre-commit in this repo to prevent commiting with errors ============
-  cp $DOTFILES_PATH/git-pre-commit-sample .git/hooks/pre-commit 
-  chmod +x $DOTFILES_PATH/.git/hooks/pre-commit
-
-  echo =========== Creating folders ===========
-  mkdir -p ~/Personal/Projects
-  mkdir -p ~/Work/Projects
+function log_info() {
+    echo "[INFO] $1"
 }
+
+function log_error() {
+    echo "[ERROR] $1" >&2
+}
+function set_home_dir() {
+    if $DRY_RUN; then
+        HOME="$EXPECTED_HOME"
+        mkdir -p "$HOME"
+        log_info "Dry run: Using $HOME as home directory"
+    fi
+}
+function install_homebrew() {
+    log_info "Installing Homebrew package manager"
+    export HOMEBREW_NO_INSTALL_FROM_API=1
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+}
+
+function install_applications() {
+    log_info "Installing applications with Homebrew"
+    local applications=(
+        git
+        nvim
+        curl
+        zsh
+    )
+    brew install -y ${applications[@]}
+}
+
+function configure_git() {
+    log_info "Configuring Git"
+    git config --global color.ui true
+    git config --global push.default simple
+}
+
+function set_zsh_as_default_shell() {
+    log_info "Setting Zsh as default shell"
+    chsh -s $(which zsh)
+}
+
+function install_oh_my_zsh() {
+    if [ ! -d "$HOME/.oh-my-zsh" ]; then
+        if $DRY_RUN; then
+            log_info "Would install Oh My Zsh"
+        else
+            sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+        fi
+    fi
+}
+
+function install_powerlevel10k() {
+    if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k" ]; then
+        if $DRY_RUN; then
+            log_info "Would install Powerlevel10k theme"
+        else
+            git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+        fi
+    fi
+}
+
+function setup_dotfiles() {
+    log_info "Setting up dotfiles"
+    git clone https://github.com/ivanlp10n2/dotfiles $DOTFILES_PATH
+    # Change to the zsh directory and run the install script
+    cd "$DOTFILES_PATH/zsh"
+    if $DRY_RUN; then
+        log_info "Would run ./install.sh --dry-run"
+    else
+        ./install.sh
+    fi
+    
+    cd - # Return to the previous directory
+}
+
+function create_project_folders() {
+    log_info "Creating project folders"
+    mkdir -p ~/Personal/Projects
+    mkdir -p ~/Work/Projects
+}
+
+function macos_installation() {
+    set_home_dir
+    install_homebrew
+    install_applications
+    configure_git
+    set_zsh_as_default_shell
+    install_oh_my_zsh
+    install_powerlevel10k
+    setup_dotfiles
+    create_project_folders
+    
+    log_info "macOS setup complete!"
+    if $DRY_RUN; then
+        log_info "Dry run completed. Check $EXPECTED_HOME for expected changes."
+        ls -la $EXPECTED_HOME
+    fi
+}
+
+# Parse command line arguments
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --dry-run) DRY_RUN=true; shift ;;
+        *) echo "Unknown parameter: $1"; exit 1 ;;
+    esac
+done
+
+# Execute main function
+macos_installation
--- update_local_functions.sh
+++ update_local_functions.sh
@@ -1,78 +1,105 @@
 #!/bin/zsh
-# usage: backups to BKUP_FOLDER and loads all configs from DOTFILES_PATH
-DOTFILES_PATH=~/.dotfiles
-BKUP_FOLDER=$DOTFILES_PATH-bak
-mkdir -p $BKUP_FOLDER
 
-function move_to_dotfiles_quietly {
-  from_path = $1
-  to_path_in_dotfiles = $DOTFILES_PATH$2
-  mv -f $from_path $to_path_in_dotfiles 2>/dev/null
+DOTFILES_PATH=~/.dotfiles
+BACKUP_FOLDER=$DOTFILES_PATH-bak
+ZSH_SRC_PATH=$DOTFILES_PATH/zsh
+
+# Utility functions
+function log_info() {
+    echo "[INFO] $1"
 }
-function copy_from_dotfiles_quietly {
-  from_path_in_dotfiles = $DOTFILES_PATH$1
-  to_path = $2
-  mv -f $from_path_in_dotfiles $to_path 2>/dev/null
+
+function log_error() {
+    echo "[ERROR] $1" >&2
 }
-function update_common_configs {
-  ZSH_SRC_PATH=$DOTFILES_PATH/zsh
-  function update_vimrc {
+
+function create_backup_folder() {
+    mkdir -p $BACKUP_FOLDER
+}
+
+function move_to_dotfiles_quietly() {
+    local from_path=$1
+    local to_path_in_dotfiles=$DOTFILES_PATH$2
+    mv -f $from_path $to_path_in_dotfiles 2>/dev/null
+}
+
+function copy_from_dotfiles_quietly() {
+    local from_path_in_dotfiles=$DOTFILES_PATH$1
+    local to_path=$2
+    cp -f $from_path_in_dotfiles $to_path 2>/dev/null
+}
+
+# Config update functions
+function update_vim_configs() {
     move_to_dotfiles_quietly ~/.vimrc /vim/
     move_to_dotfiles_quietly ~/.ideavimrc /vim/
-    cp /vim/.vimrc ~/.vimrc 2>/dev/null
-    cp /vim/.ideavimrc ~/.ideavimrc 2>/dev/null
-  }
-  function update_zshrc {
-    mv -f ~/.zshrc "$BKUP_FOLDER"/.zshrc 2>/dev/null
-    mv -f ~/.zsh_custom_cfg $BKUP_FOLDER/ 2>/dev/null
-    mv -f ~/.zshenv $BKUP_FOLDER/ 2>/dev/null
-    cp "$ZSH_SRC_PATH"/.zshrc ~/.zshrc 2>/dev/null
-    cp "$ZSH_SRC_PATH"/.zsh_custom_cfg ~/.zsh_custom_cfg 2>/dev/null
-    cp "$ZSH_SRC_PATH"/.zshenv ~/.zshenv 2>/dev/null
-  }
-  function update_power_10_k_theme {
-    mv -f ~/.p10k.zsh $BKUP_FOLDER/ 2>/dev/null
-    cp "$ZSH_SRC_PATH"/.p10k.zsh ~/.p10k.zsh 2>/dev/null
-  }
-  function update_aliases {
-    mv -f ~/.aliases $BKUP_FOLDER/ 2>/dev/null
-    cp "$ZSH_SRC_PATH"/.aliases ~/.aliases 2>/dev/null
-  }
-
-  echo ====== Backing up your cfg files to $(echo $BKUP_FOLDER) =======
-  update_vimrc
-  update_zshrc
-  update_power_10_k_theme
-  update_aliases
+    copy_from_dotfiles_quietly /vim/.vimrc ~/.vimrc
+    copy_from_dotfiles_quietly /vim/.ideavimrc ~/.ideavimrc
 }
 
-function update_tilix_configs {
-  mv -f ~/.tilix $BKUP_FOLDER/ 2>/dev/null
-
-  cp "$DOTFILES_PATH"/tilix/.tilix ~/.tilix 2>/dev/null
-  cp "$DOTFILES_PATH"/profile-config ~/profile-config 2>/dev/null
-
-  dconf load /com/gexperts/Tilix/ < $DOTFILES_PATH/tilix/tilix.dconf
+function update_zsh_configs() {
+    mv -f ~/.zshrc "$BACKUP_FOLDER"/.zshrc 2>/dev/null
+    mv -f ~/.zsh_custom_cfg $BACKUP_FOLDER/ 2>/dev/null
+    mv -f ~/.zshenv $BACKUP_FOLDER/ 2>/dev/null
+    copy_from_dotfiles_quietly "$ZSH_SRC_PATH"/.zshrc ~/.zshrc
+    copy_from_dotfiles_quietly "$ZSH_SRC_PATH"/.zsh_custom_cfg ~/.zsh_custom_cfg
+    copy_from_dotfiles_quietly "$ZSH_SRC_PATH"/.zshenv ~/.zshenv
 }
 
-function update_macos_configs {
-  update_common_configs
+function update_powerlevel10k_theme() {
+    mv -f ~/.p10k.zsh $BACKUP_FOLDER/ 2>/dev/null
+    copy_from_dotfiles_quietly "$ZSH_SRC_PATH"/.p10k.zsh ~/.p10k.zsh
 }
 
-function update_linux_configs {
-  update_common_configs
-  update_tilix_configs
+function update_aliases() {
+    mv -f ~/.aliases $BACKUP_FOLDER/ 2>/dev/null
+    copy_from_dotfiles_quietly "$ZSH_SRC_PATH"/.aliases ~/.aliases
 }
 
-function update_configs {
-  os_name=$(uname)
-  if [ "$os_name" = "Linux" ]; then
-    update_linux_configs
-  elif [ "$os_name" = "Darwin" ]; then
-    update_macos_configs
-  else
-    echo "Unknown system."
-  fi
+function update_common_configs() {
+    log_info "Backing up your config files to $BACKUP_FOLDER"
+    update_vim_configs
+    update_zsh_configs
+    update_powerlevel10k_theme
+    update_aliases
 }
 
+function update_tilix_configs() {
+    mv -f ~/.tilix $BACKUP_FOLDER/ 2>/dev/null
+    copy_from_dotfiles_quietly /tilix/.tilix ~/.tilix
+    copy_from_dotfiles_quietly /profile-config ~/profile-config
+    dconf load /com/gexperts/Tilix/ < $DOTFILES_PATH/tilix/tilix.dconf
+}
 
+function update_macos_configs() {
+    update_common_configs
+}
+
+function update_linux_configs() {
+    update_common_configs
+    update_tilix_configs
+}
+
+function update_configs() {
+    local os_name=$(uname)
+    case $os_name in
+        "Linux")
+            update_linux_configs
+            ;;
+        "Darwin")
+            update_macos_configs
+            ;;
+        *)
+            log_error "Unknown system: $os_name"
+            return 1
+            ;;
+    esac
+}
+
+function main() {
+    create_backup_folder
+    update_configs
+}
+
+# Execute main function
+main
