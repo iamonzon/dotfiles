2781f254b6e7e5e724850dc519459a0dae78521c
iamonzon <github@iamonzon.dev>
2026-01-23 18:59:21 -0300

    feat: add yazi.nvim plugin with keybindings

---
+++ lazy-vim/lua/plugins/yazi.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "mikavilpas/yazi.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    event = "VeryLazy",
+    keys = {
+      { "<leader>y", "<cmd>Yazi<cr>", desc = "Open yazi at current file" },
+      { "<leader>Y", "<cmd>Yazi cwd<cr>", desc = "Open yazi in cwd" },
+    },
+    opts = {
+      open_for_directories = true,
+    },
+  },
+}
