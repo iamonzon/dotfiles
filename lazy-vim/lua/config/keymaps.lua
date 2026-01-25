fb6dc3537b16e88f7cd5838f9849d348ca4cf65c
ivan <business.ivan.agustin@gmail.com>
2026-01-25 14:14:49 -0300

    feat: add WW/QQ save and quit keymaps
    Add convenient keymaps for quick save (WW) and quit without saving (QQ).

--- lazy-vim/lua/config/keymaps.lua
+++ lazy-vim/lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "WW", ":w<CR>", { desc = "Save file" })
+vim.keymap.set("n", "QQ", ":q!<CR>", { desc = "Quit without saving" })
