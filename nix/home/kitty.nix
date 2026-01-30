20c28f45885d5a67e192f56d8dd96342d5e81572
ivan <business.ivan.agustin@gmail.com>
2026-01-30 12:20:41 -0300

    refactor: extract kitty and tmux configs from terminal.nix
    Split terminal.nix into dedicated files for better organization:
    - kitty.nix: Kitty terminal emulator configuration
    - tmux.nix: tmux terminal multiplexer configuration
    - terminal.nix: fonts and imports only

---
+++ nix/home/kitty.nix
@@ -0,0 +1,94 @@
+{ ... }:
+
+{
+  # Kitty terminal emulator
+  programs.kitty = {
+    enable = true;
+    shellIntegration.enableZshIntegration = false; # We handle this manually to avoid tmux issues
+    font = {
+      name = "0xProto Nerd Font Mono";
+      size = 22.0;
+    };
+    settings = {
+      # Window
+      window_padding_width = 10;
+      hide_window_decorations = "titlebar-only";
+      background_opacity = "0.95";
+
+      # Terminal
+      scrollback_lines = 10000;
+      enable_audio_bell = false;
+      shell = "/bin/zsh";
+      cursor_shape = "block";
+      cursor_trail = 3;
+      cursor_trail_decay = "0.1 0.4";
+
+      # Layout - splits for iTerm-like pane behavior
+      enabled_layouts = "splits,stack";
+
+      # macOS specific
+      macos_option_as_alt = "yes";
+      macos_quit_when_last_window_closed = "yes";
+
+      # Catppuccin Mocha colors
+      foreground = "#cdd6f4";
+      background = "#1e1e2e";
+      selection_foreground = "#cdd6f4";
+      selection_background = "#585b70";
+      cursor = "#f5e0dc";
+      cursor_text_color = "#1e1e2e";
+
+      # ANSI colors
+      color0 = "#45475a";
+      color1 = "#f38ba8";
+      color2 = "#a6e3a1";
+      color3 = "#f9e2af";
+      color4 = "#89b4fa";
+      color5 = "#f5c2e7";
+      color6 = "#94e2d5";
+      color7 = "#a6adc8";
+      color8 = "#585b70";
+      color9 = "#f37799";
+      color10 = "#89d88b";
+      color11 = "#ebd391";
+      color12 = "#74a8fc";
+      color13 = "#f2aede";
+      color14 = "#6bd7ca";
+      color15 = "#bac2de";
+
+      # Inactive pane styling
+      inactive_text_alpha = "0.3";
+      active_border_color = "#89b4fa";
+      inactive_border_color = "#45475a";
+    };
+    keybindings = {
+      # Scrolling (vim-style)
+      "alt+u" = "scroll_page_up";
+      "alt+d" = "scroll_page_down";
+      "ctrl+k" = "scroll_line_up";
+      "ctrl+j" = "scroll_line_down";
+
+      # Pane navigation (Alt+h/j/k/l)
+      # "alt+h" = "neighboring_window left";
+      # "alt+j" = "neighboring_window bottom";
+      # "alt+k" = "neighboring_window top";
+      # "alt+l" = "neighboring_window right";
+
+      # # Pane splitting (Alt+Ctrl+j/l)
+      # "alt+ctrl+j" = "launch --location=hsplit --cwd=current";
+      # "alt+ctrl+l" = "launch --location=vsplit --cwd=current";
+
+      # # Pane resizing (Alt+Shift+h/j/k/l)
+      # "alt+shift+h" = "resize_window narrower";
+      # "alt+shift+j" = "resize_window shorter";
+      # "alt+shift+k" = "resize_window taller";
+      # "alt+shift+l" = "resize_window wider";
+
+      # # Maximize pane (Alt+Ctrl+x)
+      # "alt+ctrl+x" = "toggle_layout stack";
+
+      # # Close pane (Cmd+w) - override default quit behavior
+      # "cmd+w" = "close_window";
+    };
+  };
+}
