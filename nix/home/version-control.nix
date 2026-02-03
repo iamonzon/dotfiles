106b497a6f9132b71c233ea394194735539e94df
iamonzon <github@iamonzon.dev>
2026-02-03 15:52:39 -0300

    feat: add gitmux to match current command line config

--- nix/home/version-control.nix
+++ nix/home/version-control.nix
@@ -5,6 +5,7 @@
     git
     git-lfs
     gh  # GitHub CLI
+    gitmux  # Git status in tmux status bar
   ];
 
   programs.git = {
@@ -19,4 +20,37 @@
       init.defaultBranch = "main";
     };
   };
+
+  # Gitmux config (p10k-matching format)
+  xdg.configFile."gitmux/.gitmux.conf".text = ''
+    tmux:
+      symbols:
+        branch: ""
+        hashprefix: "@"
+        ahead: "⇡"
+        behind: "⇣"
+        staged: "+"
+        conflict: "~"
+        modified: "!"
+        untracked: "?"
+        stashed: "*"
+        clean: ""
+      styles:
+        clear: "#[fg=default]"
+        branch: "#[fg=green]"
+        remote: "#[fg=cyan]"
+        divergence: "#[fg=green]"
+        staged: "#[fg=yellow]"
+        conflict: "#[fg=red]"
+        modified: "#[fg=yellow]"
+        untracked: "#[fg=blue]"
+        stashed: "#[fg=green]"
+        clean: "#[fg=green]"
+      layout: [branch, divergence, " ", flags]
+      options:
+        branch_max_len: 32
+        branch_trim: right
+        ellipsis: "…"
+        hide_clean: true
+  '';
 }
