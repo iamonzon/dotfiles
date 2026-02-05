2035d8d77ef046b25d9448fa20a1fc3f77d51a94
iamonzon <github@iamonzon.dev>
2026-02-05 08:40:06 -0300

    fix: disable autoformat

--- lazy-vim/lua/config/options.lua
+++ lazy-vim/lua/config/options.lua
@@ -2,5 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- Disable format-on-save globally (manual format via <leader>lf still works)
+vim.g.autoformat = false
+
 -- Use nix-installed zsh (not /usr/bin/zsh which doesn't exist)
 vim.opt.shell = vim.fn.exepath("zsh")
