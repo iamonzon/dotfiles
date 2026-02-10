349fd102bf560a154cc44f0e9b9261a06cfae2ae
iamonzon <github@iamonzon.dev>
2026-02-09 22:18:10 -0300

    chore: move unused files to legacy folder

--- nix/flake.nix
+++ nix/flake.nix
@@ -18,32 +18,6 @@
       system = "aarch64-darwin";
       pkgs = nixpkgs.legacyPackages.${system};
 
-      # Yazi config directory with flavors
-      yaziConfigDir = pkgs.runCommand "yazi-config" {} ''
-        mkdir -p $out/flavors
-        cp ${./experiments/yazi/yazi.toml} $out/yazi.toml
-        cp ${./experiments/yazi/theme.toml} $out/theme.toml
-        cp -r ${./experiments/yazi/flavors}/* $out/flavors/
-      '';
-
-      # Preview tools for yazi
-      previewTools = with pkgs; [
-        file              # mime type detection
-        ffmpegthumbnailer # video thumbnails
-        poppler-utils     # PDF previews (pdftoppm)
-        imagemagick       # image processing
-      ];
-
-      previewPath = pkgs.lib.makeBinPath previewTools;
-
-      # Wrapper script that runs yazi with custom config
-      yaziTest = pkgs.writeShellScriptBin "yazi-test" ''
-        export YAZI_CONFIG_HOME=${yaziConfigDir}
-        export COLORTERM=truecolor
-        export PATH="${previewPath}:$PATH"
-        exec ${pkgs.yazi}/bin/yazi "$@"
-      '';
-
     in
     {
       homeConfigurations = {
@@ -54,14 +28,6 @@
             ./home/default.nix
             ./hosts/macbook.nix
           ];
-        };
-      };
-
-      # Apps for testing configurations
-      apps.${system} = {
-        yazi-test = {
-          type = "app";
-          program = pkgs.lib.getExe yaziTest;
         };
       };
     };
