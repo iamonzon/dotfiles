f3f78bfeda13cd7bc9ab31a7479d75c31ae996b0
iamonzon <github@iamonzon.dev>
2026-02-27 11:39:20 -0300

    refactor: add sdkman to path. delete coursier

--- nix/hosts/macbook.nix
+++ nix/hosts/macbook.nix
@@ -4,6 +4,10 @@
   # Darwin-specific session paths
   home.sessionPath = [
     "$HOME/.npm-global/bin"
+    "$HOME/.sdkman/bin"
+    "$HOME/.sdkman/candidates/java/current/bin"
+    "$HOME/.sdkman/candidates/sbt/current/bin"
+    "$HOME/.sdkman/candidates/scala/current/bin"
   ] ++ lib.optionals pkgs.stdenv.isDarwin [
     "/opt/homebrew/bin"
   ];
