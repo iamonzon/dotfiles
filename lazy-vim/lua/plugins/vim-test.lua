47590bb18dbc8218aadfd170c91c519063521f7f
iamonzon <ivan@boddlelearning.com>
2026-03-13 20:01:42 -0300

    feat: run LMS rspec tests through Docker in vim-test

--- lazy-vim/lua/plugins/vim-test.lua
+++ lazy-vim/lua/plugins/vim-test.lua
@@ -10,12 +10,25 @@ return {
   config = function()
     vim.g["test#strategy"] = "neovim_sticky"
 
-    -- cd to project root before running (handles relative binary paths)
+    -- LMS: detect Rails project and run rspec through Docker
+    local lms_root = vim.fn.expand("~/Work/boddle-projects/learning-management-system/main")
+
     vim.g["test#custom_transformations"] = {
-      cd_project = function(cmd)
+      docker_lms = function(cmd)
+        local file = vim.fn.expand("%:p")
+        if file:find(lms_root, 1, true) then
+          -- Strip the project root to get the relative spec path
+          local rel_cmd = cmd:gsub(lms_root .. "/", "")
+          return string.format(
+            "docker compose -f %s/docker-compose-rspec.yml run --rm lms bundle exec %s",
+            lms_root,
+            rel_cmd
+          )
+        end
+        -- Non-LMS projects: just cd and run normally
         return string.format("cd %s && %s", vim.fn.getcwd(), cmd)
       end,
     }
-    vim.g["test#transformation"] = "cd_project"
+    vim.g["test#transformation"] = "docker_lms"
   end,
 }
